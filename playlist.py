641063f7ed86dbcde23673eb77048cce475ab571
USER <noa.khatamov@gmail.com>
2025-11-14 17:04:25 +0200

    added 2 more functions: 1. check if song is in the playlist based on the name of the song. 2. a function that removes the song from the playlist

--- playlist.py
+++ playlist.py
@@ -22,6 +22,22 @@ def print_playlist(playlist):
         print()
 
 
+def check_song_exists(playlist, name):
+    if name in playlist:
+        return True
+    else:
+        return False
+
+
+def remove_song(playlist, name):
+    if check_song_exists(playlist, name):
+        playlist.pop(name)
+        print(f"'{name}' was removed from the playlist.")
+    else:
+        print(f"The song '{name}' doesn't exist in the playlist. It may have already been removed.")
+
+
+
 def main():
     counter = 0
     while counter < 3:
@@ -35,5 +51,25 @@ def main():
         add_song(liked_songs, name, artist, minutes, seconds, genre)
         counter += 1
     print_playlist(liked_songs)
+
+    while True:
+        check_song = input("What song would you like to check? (or enter 'stop' to stop checking) ")
+        if check_song == 'stop':
+            break
+        if check_song_exists(liked_songs, check_song):
+            print(f"The song '{check_song}' exists in the playlist.")
+            print()
+            remove_option = input("Do you want to remove this song? y/n: ")
+            if remove_option == 'y':
+                remove_song(liked_songs, check_song)
+                print()
+            elif remove_option == 'n':
+                print()
+        else:
+            remove_song(liked_songs, check_song)
+            print()
+
+    print_playlist(liked_songs)
+
 
 main()
